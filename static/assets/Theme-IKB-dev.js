336ed2a5ce837b06b4a00b59677215f269d196b2
planenalp <111256256+planenalp@users.noreply.github.com>
2025-04-01 04:53:19 +0800

    Update Theme-IKB-dev.js

--- static/assets/Theme-IKB-dev.js
+++ static/assets/Theme-IKB-dev.js
@@ -680,6 +680,14 @@ document.addEventListener('DOMContentLoaded', function() {
         #cmButton {
             border-radius: unset; /* 去除圆角 */
             border: unset; /* 去除边框 */
+            box-shadow: var(--box-shadow); /* 阴影 */
+            -webkit-tap-highlight-color: transparent; /* 修复某些安卓设备的点击外框 */
+            -webkit-touch-callout: none; /* 禁用 iOS 长按弹出菜单 */
+            -webkit-user-select: none; /* 禁用 iOS Safari 和其他 WebKit 内核浏览器的文本选择 */
+            -moz-user-select: none; /* 适用于 Firefox */
+            -ms-user-select: none; /* 适用于 IE10+ 和 Edge */
+            user-select: none; /* 标准语法 */
+            outline: none !important; /* 解决按压边框闪烁 */
         }
         
          /* 评论按钮悬停 */
